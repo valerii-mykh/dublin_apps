1f59fe22c7384f8b8cedcf1ca0235ec88452a74d
valerii11 <valerii_mih@ukr.net>
2021-09-21 11:23:55 +0300

    dublin_apps

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$('.slider1').owlCarousel({
+    loop:false,
+    margin:20,
+    nav: true,
+//    autoplay:true,
+//    autoplayTimeout: 1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+$('.slider3').owlCarousel({
+    loop:false,
+    margin:20,
+    nav: true,
+//    autoplay:true,
+//    autoplayTimeout: 1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
